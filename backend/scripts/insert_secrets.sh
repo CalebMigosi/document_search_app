457490c0cdaa28af8bb2d9ea1f64ce357abec945
Caleb Migosi <calebmigosi@gmail.com>
2022-11-24 19:33:16 +0100

    Jenkins test

--- backend/scripts/insert_secrets.sh
+++ backend/scripts/insert_secrets.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 curl -sSL https://install.python-poetry.org | python3
-$HOME/.local/bin/poetry run python ../app/config/create_env_file.py
+source $HOME/.local/bin/poetry run python ../app/config/create_env_file.py
