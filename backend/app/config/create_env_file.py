a2b5210e3f7e30083a208285da1c4eec1b6f43ab
Caleb Migosi <calebmigosi@gmail.com>
2022-11-24 19:59:42 +0100

    Jenkins test

--- backend/app/config/create_env_file.py
+++ backend/app/config/create_env_file.py
@@ -3,6 +3,11 @@ import os
 DIR = os.path.dirname(__file__)
 HOME_DIR = os.path.abspath(os.path.join(DIR, os.pardir, os.pardir, os.pardir))
 
-with open(os.path.join(HOME_DIR, ".env"), "x") as f:
-    for key, value in os.environ.items():
-        f.write(f"{key}={value}")
+if os.path.exists(os.path.join(HOME_DIR, ".env")):
+    with open(os.path.join(HOME_DIR, ".env"), "w") as f:
+        for key, value in os.environ.items():
+            f.write(f"{key}={value}")
+else:
+    with open(os.path.join(HOME_DIR, ".env"), "x") as f:
+        for key, value in os.environ.items():
+            f.write(f"{key}={value}")
--- backend/scripts/insert_secrets.sh
+++ backend/scripts/insert_secrets.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+chmod +x -R ${env.WORKSPACE}
+sudo apt-get update && sudo apt-get install -y curl
+sudo apt-get install python3.10
 curl -sSL https://install.python-poetry.org | python3
-source $HOME/.local/bin/poetry run python ../app/config/create_env_file.py
+cd backend
+source $HOME/.local/bin/poetry run python ./app/config/create_env_file.py
