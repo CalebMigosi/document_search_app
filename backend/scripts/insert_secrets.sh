ff2d7d07ac14a7bbf4b7db4c4bfcffb42820389c
Caleb Migosi <calebmigosi@gmail.com>
2022-11-24 22:02:43 +0100

    Jenkins File edit

--- backend/scripts/insert_secrets.sh
+++ backend/scripts/insert_secrets.sh
@@ -3,5 +3,5 @@ chmod +x -R ${env.WORKSPACE}
 sudo apt-get update && sudo apt-get install -y curl
 sudo apt-get install python3.10
 curl -sSL https://install.python-poetry.org | python3
-cd backend
+cd .. && cd .. cd backend
 $HOME/.local/bin/poetry run python ./app/config/create_env_file.py
